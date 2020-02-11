d92cda021bb9674de759eddf813664f9e6379c5c
Md Kofil Uddin <kofiluddinbasis30@gmail.com>
2020-02-11 14:39:55 +0600

    Initial Commit

---
+++ SideMenuExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenuExample
+//
+//  Created by Md Kofil Uddin on 2/11/20.
+//  Copyright © 2020 Ezze Technology Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
